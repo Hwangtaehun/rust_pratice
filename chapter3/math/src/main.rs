5bb61c78a5b50a6036a151ebfa8cbfb21ac6923c
Hwangtaehun <r283@naver.com>
2025-07-22 20:25:27 +0900

    배열까지 완료
    몸이 안좋아서 일찍 끝냄

---
+++ chapter3/math/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let sum = 5 + 10;
+    println!("sum = {sum}");
+
+    let difference = 95.5 - 4.3;
+    println!("difference = {difference}");
+
+    let product = 4 * 30;
+    println!("product = {product}");
+
+    let quotient = 56.7 / 32.2;
+    println!("quotient = {quotient}");
+
+    let truncated = -5 / 3;
+    println!("truncated = {truncated}");
+
+    let remainder = 43 % 5;
+    println!("remainder = {remainder}");
+}
